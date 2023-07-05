aa5676bf24749d34cb1bc632d28ee822f37b2c9d
MiteraI <91677179+MiteraI@users.noreply.github.com>
2023-07-05 18:39:36 +0700

    Allow worker to upload image as proof for order completion

--- src/main/java/app/vinhomes/repository/order/ScheduleRepository.java
+++ src/main/java/app/vinhomes/repository/order/ScheduleRepository.java
@@ -3,6 +3,7 @@ package app.vinhomes.repository.order;
 import app.vinhomes.entity.Order;
 import app.vinhomes.entity.order.Schedule;
 import app.vinhomes.entity.order.TimeSlot;
+import app.vinhomes.entity.type_enum.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByWorkDayBetweenAndWorkers_AccountId(LocalDate startDate, LocalDate endDate, Long workerId);
+    List<Schedule> findAllByWorkDayBetweenAndWorkers_AccountIdAndOrder_Status(LocalDate startDate, LocalDate endDate, Long workerId, OrderStatus status);
     Schedule findByOrder (Order order);
 
 }
