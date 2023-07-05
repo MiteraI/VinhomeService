0b643d969f205556d92d20d8d10004a3845497fa
MiteraI <91677179+MiteraI@users.noreply.github.com>
2023-07-05 10:22:53 +0700

    Homepage for worker is the schedule page

--- src/test/java/app/vinhomes/repository/ScheduleRepositoryTest.java
+++ src/test/java/app/vinhomes/repository/ScheduleRepositoryTest.java
@@ -1,7 +1,19 @@
 package app.vinhomes.repository;
 
+import app.vinhomes.repository.order.ScheduleRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ScheduleRepositoryTest {
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Test
+    public void printAll() {
+        System.out.println(scheduleRepository.findAll());
+    }
 
 }
