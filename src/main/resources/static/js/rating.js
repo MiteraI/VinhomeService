917ea519f4bac87a8d7a96bcb61e6e2438fd972b
trihuynh <135089955+trihtmse171368@users.noreply.github.com>
2023-07-30 22:21:49 +0700

    more minor fix

--- src/main/resources/static/js/rating.js
+++ src/main/resources/static/js/rating.js
@@ -1,7 +1,5 @@
 const allStar = document.querySelectorAll('.stars-inner')
-const avgStars = document.querySelectorAll('.avg-stars')
 const avgStarsPercentage = document.querySelectorAll('.avg-star-percentage')
-const avgRatingContainer = document.querySelectorAll('.avgRatingContainer')
 let avgRatingForEachService = []
 let avgForEachRating = {}
 
@@ -26,25 +24,15 @@ const getAllAvgRating = async () => {
         avgRatingForEachService.push(rating.data[i][j])
       }
     }
-    else {
-      for (let key in rating.data[i]) {
-        avgForEachRating[`${key}`] = rating.data[i][key].reverse()
-
-      }
-    }
   }
 
   console.log(avgForEachRating)
 
   var j = 0;
   for (let i = 0; i < allStar.length; i++) {
-    if (i % 2 == 0) {
-      let starPercentageRounded = convertToPercentage(avgRatingForEachService[j]);
-      allStar[i].style.width = starPercentageRounded
-      allStar[i + 1].style.width = starPercentageRounded
-      avgStars[j].innerHTML = `${parseFloat(avgRatingForEachService[j]).toFixed(1)} out of 5 stars`
-      j++
-    }
+    let starPercentageRounded = convertToPercentage(avgRatingForEachService[j]);
+    allStar[i].style.width = starPercentageRounded
+    j++
 
   }
 
@@ -54,28 +42,6 @@ const getAllAvgRating = async () => {
 
 getAllAvgRating()
 
-for (let i = 0; i < starsContainer.length; i++) {
-  starsContainer[i].addEventListener('mouseover', (e) => {
-    e.preventDefault()
-    avgRatingContainer[i].classList.toggle('hidden')
-    var j = 0
-    var z = 0
-    var percentage = 0
-    for (let key in avgForEachRating) {
-      for (let k = j; k < avgForEachRating[key].length + j; k++) {
-        percentage = parseFloat(avgForEachRating[key][z]).toFixed(2) * 100
-        avgStarsPercentage[k].style.width = `${percentage}%`;
-        percentageText[k].innerHTML = `${parseFloat(percentage).toFixed(0)}%`
-        z++;
-      }
-      j = z
-      z = 0
-    }
-  })
-  starsContainer[i].addEventListener('mouseout', () => {
-    avgRatingContainer[i].classList.toggle('hidden')
-  })
-}
 
 function convertToPercentage(number) {
   let starPercentage = (parseFloat(number.toFixed(1) / starTotal)) * 100;
